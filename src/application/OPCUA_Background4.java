d5ae4a8f832430643b4d6c086356718b7c54e9df
kuka <kuka@DESKTOP-VF3K647>
2023-08-14 17:29:35 +1000

    --- automatic rebase to remote; old local SHA: 742dfd769fab520d53460bc8a94c32f95028fe6b ---

---
+++ src/application/OPCUA_Background4.java
@@ -0,0 +1,62 @@
+package application;
+
+import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
+import com.kuka.roboticsAPI.applicationModel.tasks.CycleBehavior;
+import com.kuka.roboticsAPI.applicationModel.tasks.RoboticsAPICyclicBackgroundTask;
+
+public class OPCUA_Background4 extends RoboticsAPICyclicBackgroundTask {
+	
+	@Inject	
+	private OPCUA_Client_Background4 OPCUA;
+	
+	@Override
+	public void initialize(){
+
+		// Comment Between Here
+		
+		try {
+			OPCUA.SetUp();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			OPCUA.ServerUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		// AND HERE
+		
+		initializeCyclic(0, 10, TimeUnit.MILLISECONDS, CycleBehavior.BestEffort);
+	}
+
+	@Override
+	public void runCyclic() { 
+		// Comment Between Here
+		
+		
+		if (OPCUA.Disconnect==false) {
+			try {
+				OPCUA.ServerUpdate();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if (OPCUA.Disconnect==true) {
+			OPCUA.clientDisconnect();
+			while (true){
+				
+			}
+		}
+		
+		
+		
+		
+		// AND HERE
+	}
+
+}

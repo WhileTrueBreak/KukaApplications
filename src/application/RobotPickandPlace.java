237cdce4f595b484f0ea34bdfade2f31a3fec252
kuka <kuka@DESKTOP-VF3K647>
2023-08-22 10:12:59 +1000

    --- automatic rebase to remote; old local SHA: 16f7eff399659259f18f6f74b7feebf49a5e3cb7 ---

--- src/application/RobotPickandPlace.java
+++ src/application/RobotPickandPlace.java
@@ -88,6 +88,13 @@ public class RobotPickandPlace extends RoboticsAPIApplication {
 		gripper2F1.open();
 		mF.setLEDBlue(false);
 		gripper.move(lin(getApplicationData().getFrame("/P3")).setCartVelocity(100));
+		gripper.move(linRel(0, 0, -200, World.Current.getRootFrame()).setCartVelocity(50));// going down
+		gripper2F1.close();
+		mF.setLEDBlue(true);
+	    gripper.move(lin(getApplicationData().getFrame("/P3")).setCartVelocity(100));
+	    gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));// go to frame2
+	    gripper.move(linRel(0, 0, -200, World.Current.getRootFrame()).setCartVelocity(50));// going down
+		gripper2F1.open();
 		
 //		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));
 //		gripper2F1.close();

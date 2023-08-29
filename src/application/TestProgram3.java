6234e6d5e30ca8e5153f31b76f0f05ce6e23d7fc
k3ppp <102340397+k3ppp@users.noreply.github.com>
2023-08-29 15:24:32 +1000

    --- automatic rebase to remote; old local SHA: beba520d4e12d411e9fa11ecbe34909ce97bf9ac ---

---
+++ src/application/TestProgram3.java
@@ -0,0 +1,59 @@
+package application;
+
+import javax.inject.Inject;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.applicationModel.IApplicationData;
+import com.kuka.roboticsAPI.ioModel.*;
+import com.kuka.roboticsAPI.conditionModel.*;
+import com.kuka.task.ITaskLogger;
+import com.kuka.common.ThreadUtil;
+
+public class TestProgram3 extends RoboticsAPIApplication {
+    
+    @Inject
+    private ITaskLogger logger;
+    @Inject
+    private LBR robot;
+    @Inject
+    private IApplicationData data;
+    
+    
+    
+    @Override
+    public void initialize() {
+    }
+  
+    @Override
+    public void run() {
+        Main();
+    }
+    
+    private Output GetDO(int port) {
+        //Map you outputs here
+        return null;
+    }
+    
+    private Input GetDI(int port) {
+        //Map you inputs here
+        return null;
+    }
+    
+    private void Main() {
+        robot.move(lin(data.getFrame("/BASE_0/P2")).setCartVelocity(2000));
+        logger.info("PickUp - Close Gripper");
+        ThreadUtil.milliSleep(2000);
+        robot.move(lin(data.getFrame("/BASE_0/P3")).setCartVelocity(2000));
+        robot.move(lin(data.getFrame("/BASE_0/P4")).setCartVelocity(2000));
+        robot.move(lin(data.getFrame("/BASE_0/P5")).setCartVelocity(2000));
+        logger.info("Drop-OpenGripper");
+        ThreadUtil.milliSleep(2000);
+        robot.move(lin(data.getFrame("/BASE_0/P6")).setCartVelocity(2000));
+    }
+    
+    private void Sequence1() {
+        robot.move(ptp(data.getFrame("/BASE_0/P1")).setJointAccelerationRel(1.00));
+    }
+    
+}

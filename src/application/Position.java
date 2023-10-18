118b1a03a688c080d4c357b53f73005ec49cd18e
dispointmnt <bemyfwendpweaz@gmail.com>
2023-10-18 16:40:39 +1100

    --- automatic rebase to remote; old local SHA: 68fad1687319081195fa9d069d38d7e06ce8c65c ---

---
+++ src/application/Position.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Position {
+	public double x,y;
+	public Position(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+
+	
+//	public double x() {return x;};
+//	public double y() {return y;};
+//	public double z() {return z;};
+}

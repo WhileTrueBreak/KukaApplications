f18c9c19d1e56b8d08e063f627ecb65200420cbd
shank <shank@saho4003>
2023-11-20 13:20:27 +1100

    --- automatic rebase to remote; old local SHA: 9289086c6f5eb06f74f344dc13773ccd38d44abc ---

---
+++ src/application/print.java
@@ -0,0 +1,80 @@
+package application;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.conditionModel.ForceCondition;
+import com.kuka.roboticsAPI.conditionModel.JointTorqueCondition;
+import com.kuka.roboticsAPI.deviceModel.JointEnum;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.World;
+import com.kuka.task.ITaskLogger;
+import com.kuka.common.ThreadUtil;
+import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class print extends RoboticsAPIApplication {
+	@Inject
+	private LBR robot;
+
+	@Inject 
+	private Gripper2F gripper2F1;
+
+	@Inject
+	private MediaFlangeIOGroup mF;
+
+	@Inject
+	@Named("RobotiqGripper")
+	private Tool gripper;
+
+	@Inject
+	private ITaskLogger logger;
+	
+	@Override
+	public void initialize() {
+		gripper.attachTo(robot.getFlange());
+		gripper2F1.initalise();
+		gripper2F1.setSpeed(189);
+		gripper2F1.open();
+		mF.setLEDBlue(true);
+		ThreadUtil.milliSleep(200);
+		mF.setLEDBlue(false);
+		ThreadUtil.milliSleep(200);
+		
+		
+	}
+
+	@Override
+	public void run() {
+		
+		gripper.move(lin(getApplicationData().getFrame("/P6")).setCartVelocity(499));//frame1
+		mF.setLEDBlue(true);
+	    //JointTorqueCondition cond_1 = new JointTorqueCondition(JointEnum.J3, -12.0, 0.0);
+		ForceCondition cond_2 = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 30.0);
+		gripper.move(linRel(0, 0, -50, World.Current.getRootFrame()).setCartVelocity(5).breakWhen(cond_2));//going down
+		ThreadUtil.milliSleep(200);
+		mF.setLEDBlue(false);
+		//gripper2F1.open();
+
+	}
+}

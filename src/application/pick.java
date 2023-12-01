ce5373555e8c34cde3310149767c1d72404bd0b2
kuka <kuka@DESKTOP-JBLU9TO>
2023-11-30 17:51:45 -0800

    --- automatic rebase to remote; old local SHA: 8515e7abea6edb937d415a6e68c885843f4be1ce ---

---
+++ src/application/pick.java
@@ -0,0 +1,95 @@
+package application;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.capabilities.honk.IHonkCapability;
+import com.kuka.roboticsAPI.conditionModel.ForceCondition;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.deviceModel.kmp.SunriseOmniMoveMobilePlatform;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.World;
+import com.kuka.task.ITaskLogger;
+import com.kuka.common.ThreadUtil;
+import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class pick extends RoboticsAPIApplication {
+	@Inject
+	private LBR robot;
+
+	@Inject 
+	private Gripper2F gripper2F1;
+
+	@Inject
+	private MediaFlangeIOGroup mF;
+
+	@Inject
+	@Named("RobotiqGripper")
+	private Tool gripper;
+
+	@Inject
+	private ITaskLogger logger;
+	
+	@Inject 
+	private SunriseOmniMoveMobilePlatform kmp;
+	
+	@Override
+	public void initialize() {
+		gripper.attachTo(robot.getFlange());
+		gripper2F1.initalise();
+		gripper2F1.setSpeed(189);
+		gripper2F1.open();
+		mF.setLEDBlue(true);
+		ThreadUtil.milliSleep(200);
+		mF.setLEDBlue(false);
+		ThreadUtil.milliSleep(200);
+		
+		//FORCE CONDITIONS EXAMPLE
+		ForceCondition touch10 = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"),10 );
+		ForceCondition touch15 = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"),15 );
+		//USAGE, will move to next line when triggered
+		//LOOK at pipecutting.java for examples on analysing the break condition. 
+		//gripper.move(linRel(0, 0, -30, World.Current.getRootFrame()).setCartVelocity(50).breakWhen(touch10)); 
+	}
+
+	@Override
+	public void run() {
+		IHonkCapability honkCapability = kmp.getCapability(IHonkCapability.class);
+		honkCapability.honk();
+		gripper2F1.close();
+		mF.setLEDBlue(true);
+		ThreadUtil.milliSleep(200);
+		gripper2F1.open();
+
+		mF.setLEDBlue(false);
+		ThreadUtil.milliSleep(200);
+
+		gripper.move(lin(getApplicationData().getFrame("/P4")).setCartVelocity(200));//frame4
+		gripper.move(lin(getApplicationData().getFrame("/P5")).setCartVelocity(200));//frame5(going down)
+		gripper2F1.close();
+		mF.setLEDBlue(true);
+		gripper.move(lin(getApplicationData().getFrame("/P4")).setCartVelocity(100));//get back to frame4
+		gripper.move(lin(getApplicationData().getFrame("/DrivePos")).setCartVelocity(200));// go to DrivePos frame
+		
+	}
+}

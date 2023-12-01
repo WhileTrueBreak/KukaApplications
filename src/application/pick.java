2127d34619ef251a8c2531fac82bd9aaea419d60
kuka <kuka@DESKTOP-JBLU9TO>
2023-11-30 18:51:07 -0800

    --- automatic rebase to remote; old local SHA: 40ee9b07a9fbb34903ad3bb49ca03577f75e999a ---

--- src/application/pick.java
+++ src/application/pick.java
@@ -87,7 +87,7 @@ public class pick extends RoboticsAPIApplication {
 		
 		for (int i = 0; i <= 500; i = i+100){
 			gripper.move(ptp(getApplicationData().getFrame("/P4")).setJointVelocityRel(0.4));//frame4
-			gripper.move(linRel(-i, 0, 0, World.Current.getRootFrame()).setCartVelocity(50));//going left/right
+			gripper.move(linRel(i, 0, 0, World.Current.getRootFrame()).setCartVelocity(50));//going left/right
 			gripper.move(linRel(0, 0, -130, World.Current.getRootFrame()).setCartVelocity(50));//going left/right
 			gripper2F1.close();
 			mF.setLEDBlue(true);
@@ -96,6 +96,7 @@ public class pick extends RoboticsAPIApplication {
 			gripper.move(linRel(-i, 0, 0, World.Current.getRootFrame()).setCartVelocity(50));//going left/right
 			gripper.move(linRel(0, 0, -150, World.Current.getRootFrame()).setCartVelocity(50));//going left/right
 			gripper2F1.open();
+			gripper.move(linRel(0, 0, 150, World.Current.getRootFrame()).setCartVelocity(50));//going left/right
 		}
 	
 		

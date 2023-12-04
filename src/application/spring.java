d9c848c6c053b1027501caec503bfe61c13c605f
shank <shank@saho4003>
2023-12-04 11:32:18 +1100

    --- automatic rebase to remote; old local SHA: 2c5e16c05f981224d1feeb9a3fe5131f23aa2f83 ---

--- src/application/spring.java
+++ src/application/spring.java
@@ -83,9 +83,8 @@ public class spring extends RoboticsAPIApplication {
 	@Override
 	public void run(){
 //					gripper2F1.close();
-					gripper.move(ptp(0.0,-0.785398,0.0,1.13446,0.0,0.436332,-1.5708).setJointVelocityRel(0.2));//.setMode(springRobot));
+					gripper.move(ptp(0.0,-0.785398,0.0,1.13446,0.0,0.436332,-1.5708).setJointVelocityRel(0.2).setMode(springRobot));
 					m1 = gripper.moveAsync(positionHold(springRobot, -1, TimeUnit.SECONDS));
-				m1.cancel();
 				logger.info("Communication Signals Reset");// End If (Start)
 	}//END RUN
 }

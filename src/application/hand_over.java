aeedfd39f0bc3377b0f3e9007e57d003c8d444d1
shank <shank@saho4003>
2023-12-05 17:13:22 +1100

    --- automatic rebase to remote; old local SHA: 27de9465dad86873864e95501b1fabb48c5995af ---

--- src/application/hand_over.java
+++ src/application/hand_over.java
@@ -118,7 +118,7 @@ public class hand_over extends RoboticsAPIApplication {
 ////////////
 		
 		//TCP
-		ForceCondition condition = ForceCondition.createSpatialForceCondition(gripper.getFrame("/HAND_OVER"), 17.0);
+		ForceCondition condition = ForceCondition.createSpatialForceCondition(gripper.getFrame("/HAND_OVER"), 16.0);
 
 		ICallbackAction Action = new ICallbackAction() {
 			@Override

1d0e69bbd8a2f0059f1e514bb764b18bed5e5567
fin <fin@LAPTOP-PUV86KGE>
2023-12-05 15:03:34 +1100

    --- automatic rebase to remote; old local SHA: 562de12b4cd2066ca827f4cb3207fe98b73276cd ---

--- src/application/IndirectHandoverFinlay.java
+++ src/application/IndirectHandoverFinlay.java
@@ -71,13 +71,13 @@ public class IndirectHandoverFinlay extends RoboticsAPIApplication {
 	public void run() {
 
 		// part 1
-		gripper.move(ptp(getApplicationData().getFrame("/PART_1/p1_transition")).setJointVelocityRel(0.3));//frame1
-		gripper.move(ptp(getApplicationData().getFrame("/PART_1")).setJointVelocityRel(0.3));//frame1
+		robot.move(ptp(getApplicationData().getFrame("/PART_1/p1_transition")).setJointVelocityRel(0.3));//frame1
+		robot.move(ptp(getApplicationData().getFrame("/PART_1")).setJointVelocityRel(0.3));//frame1
 		gripper2F1.close();
 		mF.setLEDBlue(true);
-		gripper.move(ptp(getApplicationData().getFrame("/PART_1/p1_transition")).setJointVelocityRel(0.3));//frame1
-		gripper.move(ptp(getApplicationData().getFrame("/drop_point/drop_transition")).setJointVelocityRel(0.3));//frame1
-		gripper.move(ptp(getApplicationData().getFrame("/drop_point")).setJointVelocityRel(0.3));//frame1
+		robot.move(ptp(getApplicationData().getFrame("/PART_1/p1_transition")).setJointVelocityRel(0.3));//frame1
+		robot.move(ptp(getApplicationData().getFrame("/drop_point/drop_transition")).setJointVelocityRel(0.3));//frame1
+		robot.move(ptp(getApplicationData().getFrame("/drop_point")).setJointVelocityRel(0.3));//frame1
 		gripper2F1.open();
 		mF.setLEDBlue(true);
 		

4b352474891133c1546f07cce6f971a68600b074
shank <shank@saho4003>
2023-12-06 14:07:59 +1100

    --- automatic rebase to remote; old local SHA: 6f21d95bc5624e8905fe8f31694c2499258b106c ---

--- src/application/RobotPickandPlace.java
+++ src/application/RobotPickandPlace.java
@@ -6,23 +6,14 @@ import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
  
 import com.kuka.roboticsAPI.capabilities.honk.IHonkCapability;
-import com.kuka.roboticsAPI.conditionModel.BooleanIOCondition;
 import com.kuka.roboticsAPI.conditionModel.ForceCondition;
-import com.kuka.roboticsAPI.conditionModel.ICallbackAction;
-import com.kuka.roboticsAPI.conditionModel.ITriggerAction;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.deviceModel.kmp.SunriseOmniMoveMobilePlatform;
-import com.kuka.roboticsAPI.executionModel.IFiredTriggerInfo;
 import com.kuka.roboticsAPI.geometricModel.Tool;
 import com.kuka.roboticsAPI.geometricModel.World;
-import com.kuka.roboticsAPI.geometricModel.math.Vector;
-import com.kuka.roboticsAPI.sensorModel.ForceSensorData;
-import com.kuka.roboticsAPI.sensorModel.TorqueSensorData;
 import com.kuka.task.ITaskLogger;
 import com.kuka.common.ThreadUtil;
 import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
-import static com.kuka.roboticsAPI.motionModel.HRCMotions.*;
-
  
 /**
 * Implementation of a robot application.
@@ -83,98 +74,43 @@ public class RobotPickandPlace extends RoboticsAPIApplication {
 		IHonkCapability honkCapability = kmp.getCapability(IHonkCapability.class);
 		honkCapability.honk();
 		gripper2F1.close();
+		mF.setLEDBlue(true);
+		ThreadUtil.milliSleep(200);
+		gripper2F1.open();
  
 		mF.setLEDBlue(false);
 		ThreadUtil.milliSleep(200);
  
-//		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));//frame1
-//	    gripper.move(linRel(0, 0, -30, World.Current.getRootFrame()).setCartVelocity(50));//going down
-//		gripper2F1.close();
-//		mF.setLEDBlue(true);
-//		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(100));//get back to frame1
-//		gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(200));// go to frame2
-//	    gripper.move(linRel(0, 0, -90, World.Current.getRootFrame()).setCartVelocity(50));// going down
-//		gripper2F1.open();
-//		mF.setLEDBlue(false);
-//	    gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(100));
-// 
-//		
-//		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));//frame1
-//	    gripper.move(linRel(0, 0, -60, World.Current.getRootFrame()).setCartVelocity(50));//going down
-//		gripper2F1.close();
-//		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(100));//get back to frame1
-//		gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(200));// go to frame2
-//	    gripper.move(linRel(0, 0, -60, World.Current.getRootFrame()).setCartVelocity(50));// going down
-//		gripper2F1.open();
-//	    gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(100));
-//		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));//frame1
-//	    gripper.move(linRel(0, 0, -90, World.Current.getRootFrame()).setCartVelocity(50));//going down
-//		gripper2F1.close();
-//		mF.setLEDBlue(true);
-//		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(100));//get back to frame1
-//		gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(200));// go to frame2
-//	    gripper.move(linRel(0, 0, -30, World.Current.getRootFrame()).setCartVelocity(50));// going down
-//		gripper2F1.open();
-//		mF.setLEDBlue(false);
-//	    gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(100));
-//	    robot.move(lin(getApplicationData().getFrame("/DrivePos")).setCartVelocity(100));
+		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));//frame1
+	    gripper.move(linRel(0, 0, -30, World.Current.getRootFrame()).setCartVelocity(50));//going down
+		gripper2F1.close();
+		mF.setLEDBlue(true);
+		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(100));//get back to frame1
+		gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(200));// go to frame2
+	    gripper.move(linRel(0, 0, -90, World.Current.getRootFrame()).setCartVelocity(50));// going down
+		gripper2F1.open();
+		mF.setLEDBlue(false);
+	    gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(100));
+ 
 		
-		/// try this hand guiding mode ///
-//		double x = 0.3;
-//		double y = 0.3;
-//		robot.move(ptp(getApplicationData().getFrame("/P1")).setJointVelocityRel(x));
-//		robot.move(handGuiding().setJointVelocityLimit(x));
-//		//robot.setESMState("1");
-//		robot.move(ptp(getApplicationData().getFrame("/P2")));
-//		
-		/// try circ and spline motions///
-		
-		// // ///// force conditions / torquea ////////
-//		double forceInX = 0;
-//		double forceInY = 0;
-//		double forceInZ = 0;
-//		
-//		while (forceInX < 10) {
-//			logger.info("No force in X ditected"+forceInX);
-//			mF.setLEDBlue(false);
-//			ForceSensorData data = robot.getExternalForceTorque(robot.getFlange(),World.Current.getRootFrame());
-//			Vector force = data.getForce();
-//			forceInX = force.getX();
-//			ThreadUtil.milliSleep(200);
-//		}
-//		gripper2F1.open();
-//		logger.info("Force in X ditected"+forceInX);
-		
-		//
-////		
-//		ForceCondition condition = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 10.0);
-//
-//		
-//		ICallbackAction Action = new ICallbackAction() {
-//			@Override
-//			public void onTriggerFired(IFiredTriggerInfo triggerInformation) {
-//			 //toggle output state when trigger fired
-//				gripper2F1.open();
-//				logger.info("yay");
-//			}
-//		};
-//		
-//
-//		robot.move(linRel(0, 0, -60, World.Current.getRootFrame()).setCartVelocity(10).triggerWhen(condition, Action));
-//		robot.move(linRel(0, 0, 60, World.Current.getRootFrame()).setCartVelocity(10));
-//		
-//		ForceCondition cond_2 = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 10.0);
-//		gripper.move(linRel(0, 400, 0, World.Current.getRootFrame()).setCartVelocity(20).breakWhen(cond_2));//going straight
-//		if (cond_2 == null){
-//			logger.info("No Collision Detected");
-//			mF.setLEDBlue(false);
-//			ThreadUtil.milliSleep(200);
-//		}
-//		else{
-//			logger.info("Collision Detected");
-//			mF.setLEDBlue(true);
-//			ThreadUtil.milliSleep(200);
-//		}
-
+		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));//frame1
+	    gripper.move(linRel(0, 0, -60, World.Current.getRootFrame()).setCartVelocity(50));//going down
+		gripper2F1.close();
+		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(100));//get back to frame1
+		gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(200));// go to frame2
+	    gripper.move(linRel(0, 0, -60, World.Current.getRootFrame()).setCartVelocity(50));// going down
+		gripper2F1.open();
+	    gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(100));
+		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(200));//frame1
+	    gripper.move(linRel(0, 0, -90, World.Current.getRootFrame()).setCartVelocity(50));//going down
+		gripper2F1.close();
+		mF.setLEDBlue(true);
+		gripper.move(lin(getApplicationData().getFrame("/P1")).setCartVelocity(100));//get back to frame1
+		gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(200));// go to frame2
+	    gripper.move(linRel(0, 0, -30, World.Current.getRootFrame()).setCartVelocity(50));// going down
+		gripper2F1.open();
+		mF.setLEDBlue(false);
+	    gripper.move(lin(getApplicationData().getFrame("/P2")).setCartVelocity(100));
+	    robot.move(lin(getApplicationData().getFrame("/DrivePos")).setCartVelocity(100));
 	}
 }

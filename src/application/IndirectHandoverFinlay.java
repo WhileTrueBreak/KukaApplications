4757556d0cca4d0c79ecbf7633b0decc8c67a925
fin <fin@LAPTOP-PUV86KGE>
2023-12-06 12:52:31 +1100

    --- automatic rebase to remote; old local SHA: 5eab51cc69630abb477da7ec7d59b69d235a7c62 ---

--- src/application/IndirectHandoverFinlay.java
+++ src/application/IndirectHandoverFinlay.java
@@ -81,6 +81,14 @@ public class IndirectHandoverFinlay extends RoboticsAPIApplication {
 		robot.move(ptp(getApplicationData().getFrame("/drop_point/drop_transition")).setJointVelocityRel(0.3));//frame1
 		robot.move(ptp(getApplicationData().getFrame("/drop_point")).setJointVelocityRel(0.3));//frame1
 		gripper2F1.open();
+		robot.move(ptp(getApplicationData().getFrame("/drop_point/drop_transition")).setJointVelocityRel(0.3));//frame1
+		robot.move(ptp(getApplicationData().getFrame("/drop_point")).setJointVelocityRel(0.3));//frame1
+		gripper2F1.close();
+		robot.move(ptp(getApplicationData().getFrame("/PART_3/p3_transition")).setJointVelocityRel(0.3));
+		robot.move(ptp(getApplicationData().getFrame("/PART_3")).setJointVelocityRel(0.3));//frame1
+		gripper2F1.setPos(147);
+		robot.move(ptp(getApplicationData().getFrame("/PART_3/p3_transition")).setJointVelocityRel(0.3));
+		
 		
 
 		robot.move(ptp(getApplicationData().getFrame("/PART_4/p4_transition")).setJointVelocityRel(0.3));

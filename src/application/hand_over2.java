b714cab2958b24e1d530d382051ba8d099a5c0c4
shank <shank@saho4003>
2023-12-06 16:27:32 +1100

    --- automatic rebase to remote; old local SHA: 00cdb99cf5df12fb2d132acf4d85cac12f19b1c5 ---

--- src/application/hand_over2.java
+++ src/application/hand_over2.java
@@ -193,7 +193,7 @@ public class hand_over2 extends RoboticsAPIApplication {
 		double PosY_pre = PosY;
 		double PosZ_pre = PosZ;
 		
-		robot.moveAsync(positionHold(springRobot, 1, TimeUnit.SECONDS));
+		robot.moveAsync(positionHold(springRobot, 2, TimeUnit.SECONDS));
 		
 		boolean condition = false;
 		
@@ -211,7 +211,7 @@ public class hand_over2 extends RoboticsAPIApplication {
 		}
 		
 		robot.move(lin(getApplicationData().getFrame("/HAND_OVER")).setJointVelocityRel(0.4));
-		robot.moveAsync(positionHold(springRobot, 1, TimeUnit.SECONDS));
+		robot.moveAsync(positionHold(springRobot, 2, TimeUnit.SECONDS));
 		logger.info("smak me to grab :)");
 		condition = false;
 		while (condition != true) {

62edc507f82b26a6964c56d531333ba7f8242b83
kuka <kuka@DESKTOP-JBLU9TO>
2024-01-14 22:06:40 -0800

    --- automatic rebase to remote; old local SHA: 00071eed9b0999dc20089847b121fb11b4de506a ---

--- src/application/Drawerer.java
+++ src/application/Drawerer.java
@@ -311,7 +311,7 @@ public class Drawerer extends RoboticsAPIApplication{
 		
 		logger.info("Reading Path File");
 		String resPath = FileReader.findUniqueFolder("res", "..");
-		List<String> file = FileReader.readFile(resPath+"/font/f.txt");
+		List<String> file = FileReader.readFile(resPath+"/font/B.txt");
 
 //		PathPlan plan = createPathPlanV1(file, originFrame, canvas);
 		PathPlan plan = createPathPlanV2(file, originFrame, canvas);

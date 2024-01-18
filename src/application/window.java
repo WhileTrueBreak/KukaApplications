8cd0f8ff190dacc8777ee6a7523ddf4fbb9798ec
Sri <Sri@saho4003>
2024-01-18 11:55:35 +1100

    --- automatic rebase to remote; old local SHA: 365b76ad9b62f0da6deb2e6256875fa647915f4f ---

--- src/application/window.java
+++ src/application/window.java
@@ -163,10 +163,10 @@ public class window extends RoboticsAPIApplication{
 		robot.move(ptp(getApplicationData().getFrame("/windowHandle")).setJointVelocityRel(0.5));
 		robot.move(linRel(0, 0, 30).setJointVelocityRel(0.3));
 		
-		ForceCondition force = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 500);
+		//ForceCondition force = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 500);
 	
 		logger.info("moving on a line");
-		robot.move(linRel(openLine.getX(), openLine.getY(), openLine.getZ()).setCartVelocity(5).setCartAcceleration(10).breakWhen(force));
+		robot.move(linRel(openLine.getX(), openLine.getY(), openLine.getZ()).setCartVelocity(5).setCartAcceleration(10));
 		logger.info("force detected");
 	}
 }

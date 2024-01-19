c9e96b08b3dccc73335e62ca4128c2479e544394
Sri <Sri@saho4003>
2024-01-19 16:51:31 +1100

    --- automatic rebase to remote; old local SHA: 15d254111a70f3ca96019803b3ed19e6227a53ec ---

--- src/application/window.java
+++ src/application/window.java
@@ -167,13 +167,16 @@ public class window extends RoboticsAPIApplication{
 //		robot.move(linRel(0, 0, 15).setJointVelocityRel(0.3));
 //		gripper2F1.close();
 		
-		Vector3D diag = openLine.getA().multiply(-30);
+		Vector3D diag = openLine.getA().multiply(-50);
 		logger.info("moving on a line");
 		gripper.move(linRel(diag.getX(), diag.getZ(), diag.getY()).setCartVelocity(50).setCartAcceleration(5));
 		gripper.move(linRel(diag.getX(), diag.getZ(), diag.getY()).setCartVelocity(50).setCartAcceleration(5));
 
 		gripper.move(linRel(diag.getX(), diag.getZ(), diag.getY()).setCartVelocity(50).setCartAcceleration(5));
 
+		gripper.move(linRel(diag.getX(), diag.getZ(), diag.getY()).setCartVelocity(50).setCartAcceleration(5));
+		gripper.move(linRel(diag.getX(), diag.getZ(), diag.getY()).setCartVelocity(50).setCartAcceleration(5));
+		gripper.move(linRel(diag.getX(), diag.getZ(), diag.getY()).setCartVelocity(50).setCartAcceleration(5));
 		gripper.move(linRel(diag.getX(), diag.getZ(), diag.getY()).setCartVelocity(50).setCartAcceleration(5));
 	}
 }

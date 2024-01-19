480cdd1915bc5fb045315860eda6a00b5329590d
Sri <Sri@saho4003>
2024-01-19 14:44:58 +1100

    --- automatic rebase to remote; old local SHA: a4512b67362ef50fa0822885f83cd206a959a7f8 ---

--- src/application/window.java
+++ src/application/window.java
@@ -143,14 +143,14 @@ public class window extends RoboticsAPIApplication{
 		//getting the vector
 		robot.move(ptp(getApplicationData().getFrame("/windowHandle/P1")).setJointVelocityRel(0.5));
 		logger.info("Calibrating vector point 1");
-		Vector3D origin = frameToVector(calibrateFrame(gripper,20));
+		Vector3D origin = frameToVector(calibrateFrame(gripper,18));
 		logger.info(String.format("Origin: %s", origin.toString()));
 
 		logger.info("Moving to left");
 		robot.move(ptp(getApplicationData().getFrame("/windowHandle/P2")).setJointVelocityRel(0.5));
 		logger.info("Calibrating vector point 2");
 		ThreadUtil.milliSleep(1000);
-		Vector3D right = frameToVector(calibrateFrame(gripper,25));
+		Vector3D right = frameToVector(calibrateFrame(gripper,23));
 		logger.info(String.format("Right: %s", right.toString()));
 			
 		robot.move(linRel(0, 0, -20).setJointVelocityRel(0.2));
@@ -161,10 +161,10 @@ public class window extends RoboticsAPIApplication{
 		
 		robot.move(ptp(getApplicationData().getFrame("/P1")).setJointVelocityRel(0.5));
 		robot.move(ptp(getApplicationData().getFrame("/windowHandle")).setJointVelocityRel(0.5));
-		ForceCondition force = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 40);
+		ForceCondition force = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 50);
 		robot.move(linRel(0, 0, 30).setJointVelocityRel(0.3).breakWhen(force));
 		robot.move(linRel(0, 0, -2).setJointVelocityRel(0.3).breakWhen(force));
-		Vector3D openLine = openvector.multiply(70);
+		Vector3D openLine = openvector.multiply(50);
 		logger.info("moving on a line");
 		robot.move(linRel(openLine.getX(), openLine.getY(), openLine.getZ()).setCartVelocity(50).setCartAcceleration(5));
 		robot.move(linRel(openLine.getX(), openLine.getY(), openLine.getZ()).setCartVelocity(50).setCartAcceleration(5));

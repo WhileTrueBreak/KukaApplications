9bd986b55f99e40ae405df3ddc635ef8740c974e
Sri <Sri@saho4003>
2024-01-19 14:39:42 +1100

    --- automatic rebase to remote; old local SHA: f8147e3d64b09a626f53fa7110ab990c2b566eea ---

--- src/application/window.java
+++ src/application/window.java
@@ -161,10 +161,10 @@ public class window extends RoboticsAPIApplication{
 		
 		robot.move(ptp(getApplicationData().getFrame("/P1")).setJointVelocityRel(0.5));
 		robot.move(ptp(getApplicationData().getFrame("/windowHandle")).setJointVelocityRel(0.5));
-		ForceCondition force = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 30);
+		ForceCondition force = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 40);
 		robot.move(linRel(0, 0, 30).setJointVelocityRel(0.3).breakWhen(force));
 		robot.move(linRel(0, 0, -2).setJointVelocityRel(0.3).breakWhen(force));
-		Vector3D openLine = openvector.multiply(150);
+		Vector3D openLine = openvector.multiply(70);
 		logger.info("moving on a line");
 		robot.move(linRel(openLine.getX(), openLine.getY(), openLine.getZ()).setCartVelocity(50).setCartAcceleration(5));
 		robot.move(linRel(openLine.getX(), openLine.getY(), openLine.getZ()).setCartVelocity(50).setCartAcceleration(5));

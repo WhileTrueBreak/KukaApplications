605a9a45586f93eadf7b66ebd4628255247c95fa
Sri <Sri@saho4003>
2024-01-22 11:52:37 +1100

    --- automatic rebase to remote; old local SHA: b1c1b7c47c3047be4c155aba7d40e5b9fad1cc32 ---

--- src/application/window.java
+++ src/application/window.java
@@ -158,8 +158,20 @@ public class window extends RoboticsAPIApplication{
 		robot.move(mySpline.setJointVelocityRel(0.4));			
 				
 				
-		ForceCondition touch = ForceCondition.createSpatialForceCondition(gripper.getFrame("/TCP"), 35);
-		gripper.move(linRel(0, 0, 100, gripper.getFrame("/TCP")).setCartVelocity(30).breakWhen(touch));
+		Boolean con1 = true;
+		while (con1) {
+			ForceSensorData data = robot.getExternalForceTorque(robot.getFlange(),World.Current.getRootFrame());
+			Vector vForce = data.getForce();
+			double forceInY = vForce.getY();
+			forceInY = Math.abs(forceInY);
+			if (forceInY < 25){
+				robot.move(linRel(0, 0, 1).setJointVelocityRel(0.3));
+			} else {
+				con1 = false;
+				break;
+			}
+		}
+		
 		
 		robot.move(linRel(0, 0, -10).setJointVelocityRel(0.3));
 		gripper2F1.setPos(20);

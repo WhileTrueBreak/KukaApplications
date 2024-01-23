b5d81862ff3712a4fa92068624b4b87e3571774a
Sri <Sri@saho4003>
2024-01-23 11:52:08 +1100

    --- automatic rebase to remote; old local SHA: 735f653e8282cf67823f0fc5f6034711c700b355 ---

--- src/application/window.java
+++ src/application/window.java
@@ -178,16 +178,16 @@ public class window extends RoboticsAPIApplication{
 		ThreadUtil.milliSleep(100);
 		gripper2F1.close();
 		
-		springRobot.parametrize(CartDOF.X).setStiffness(2000);
-		springRobot.parametrize(CartDOF.Y).setStiffness(2000);
-		springRobot.parametrize(CartDOF.Z).setStiffness(2000);
+		springRobot.parametrize(CartDOF.X).setStiffness(3000);
+		springRobot.parametrize(CartDOF.Y).setStiffness(3000);
+		springRobot.parametrize(CartDOF.Z).setStiffness(3000);
 
 		// Stiff rotation
-		springRobot.parametrize(CartDOF.C).setStiffness(300);
-		springRobot.parametrize(CartDOF.B).setStiffness(300);
-		springRobot.parametrize(CartDOF.A).setStiffness(300);
+		springRobot.parametrize(CartDOF.C).setStiffness(100);
+		springRobot.parametrize(CartDOF.B).setStiffness(100);
+		springRobot.parametrize(CartDOF.A).setStiffness(100);
 		springRobot.setReferenceSystem(World.Current.getRootFrame());
-		springRobot.parametrize(CartDOF.ALL).setDamping(0.4);
+		springRobot.parametrize(CartDOF.ALL).setDamping(0.8);
 		
 		Vector3D diag = openLine.getA().multiply(650);
 		logger.info("moving on a line");

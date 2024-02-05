4fe19282f51d30e57d55d6588f6d176fdec83d36
Sri <Sri@saho4003>
2024-02-05 16:13:21 +1100

    --- automatic rebase to remote; old local SHA: 15925e9effd108940562a3b0f62cad8ad31fdbbb ---

--- src/application/RobotCharging.java
+++ src/application/RobotCharging.java
@@ -1,20 +1,37 @@
 package application;
 
 
+import java.util.Map;
+
 import javax.inject.Inject;
 
-import com.kuka.nav.line.VirtualLineMotion;
+import com.kuka.basictoolbox.container.IContainer;
+import com.kuka.common.ThreadUtil;
+import com.kuka.generated.ioAccess.Gripper2FIOGroup;
+import com.kuka.generated.ioAccess.RemoteControlIOGroup;
+import com.kuka.nav.Pose;
+import com.kuka.nav.data.LocationData;
+import com.kuka.nav.robot.MobileRobot;
+import com.kuka.nav.fleet.FleetManager;
+import com.kuka.resource.IResourceManager;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
-import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 
 import com.kuka.roboticsAPI.capabilities.floorMountedCharge.ChargingType;
 import com.kuka.roboticsAPI.capabilities.floorMountedCharge.IFloorMountedChargeCapability;
-import com.kuka.roboticsAPI.capabilities.interfaces.ICapability;
-import com.kuka.roboticsAPI.commandModel.IAction;
-import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.capabilities.interfaces.ILEDStripControlCapability;
+import com.kuka.roboticsAPI.capabilities.ledStrip.LEDStripUserStatePriority;
+import com.kuka.roboticsAPI.capabilities.ledStrip.SegmentColor;
+import com.kuka.roboticsAPI.controllerModel.sunrise.ISafetyState;
+import com.kuka.roboticsAPI.controllerModel.sunrise.state.kmp.IMobilePlatformBatteryState;
+import com.kuka.roboticsAPI.deviceModel.OperationMode;
 import com.kuka.roboticsAPI.deviceModel.kmp.KmpOmniMove;
 import com.kuka.roboticsAPI.deviceModel.kmp.SunriseOmniMoveMobilePlatform;
-import com.kuka.roboticsAPI.motionModel.kmp.MobilePlatformRelativeMotion;
+import com.kuka.roboticsAPI.geometricModel.LoadData;
+import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
+import com.kuka.task.ITaskLogger;
+import com.kuka.task.ITaskManager;
+import com.kuka.roboticsAPI.motionModel.kmp.MobilePlatformPosition;
+import com.kuka.roboticsAPI.motionModel.kmp.MobilePlatformParameterSet;
 
 /**
  * Implementation of a robot application.
@@ -37,11 +54,37 @@ import com.kuka.roboticsAPI.motionModel.kmp.MobilePlatformRelativeMotion;
 public class RobotCharging extends RoboticsAPIApplication {
 
 	@Inject
-	private KmpOmniMove base;
+	private KmpOmniMove kmp1;
+
+	@Inject
+	private MobileRobot MR;
 	
 	@Inject
 	private SunriseOmniMoveMobilePlatform kmp;
-
+	
+	@Inject
+	private MobilePlatformPosition MPM;
+	
+	@Inject
+	private ILEDStripControlCapability ledStrip;
+	
+	@Inject
+	private ITaskLogger logger;
+	
+	@Inject
+	private FleetManager FM;
+	
+	@Inject
+    private LocationData locData;
+	
+	@Inject
+	private RemoteControlIOGroup RCIO;
+	
+	private final static String informationText=
+	         "Robot going to charge!"+ "\n" +
+				"\n" +
+				"press cancel to cancel.";
+	
 	@Override
 	public void initialize() {
 		// initialize your application here
@@ -49,12 +92,63 @@ public class RobotCharging extends RoboticsAPIApplication {
 
 	@Override
 	public void run() {
-		// Charges with floor contacts for 10 minutes.
-		int timeoutInSeconds = 3600; // value in seconds
 		
-		IFloorMountedChargeCapability chargeCapability = kmp.getCapability(IFloorMountedChargeCapability.class);
-		chargeCapability.enableCharge(ChargingType.FOR_GIVEN_TIME,timeoutInSeconds);
-		// kmp.enableCharge(ChargingType.FOR_GIVEN_TIME, timeoutInSeconds);
+		//IMobilePlatformBatteryState battStatus = kmp.getMobilePlatformBatteryState();
+		long batteryLevel = kmp.getMobilePlatformBatteryState().getStateOfCharge();
+		Boolean chargeEnableState = kmp.getMobilePlatformBatteryState().isChargingEnabled();
+		
+		boolean motion = kmp.isMotionEnabled();
+		Map<String, Object> frame = kmp.getAllUserParameters();
+		double[] Jpos = kmp.getCurrentJointPosition();
+		LoadData Load = kmp.getLoadData();
+		
+		OperationMode opMode = kmp.getOperationMode();
+		ISafetyState safe = kmp.getSafetyState();
+		
+		logger.info("batteryLevel is" + batteryLevel);
+		logger.info("charge enabled" + chargeEnableState);
+		logger.info("motion enable" + motion);
+		logger.info("frame" + frame);
+		logger.info("J pos" + Jpos);
+		logger.info("Load" + Load);
+		logger.info("opMode is" + opMode);
+		logger.info("safety" + safe);
+		
+		//////////
+		int mod = RCIO.getMode();
+		logger.info("mod is" + mod);
+		
+		int graph = FM.getGraph().getMapId();
+		logger.info("graph" + graph);
+		
+		//////////
+		double posX = MPM.getX();
+		double posY = MPM.getY();
+		double posT = MPM.getTheta();
+		logger.info("X" + posX);
+		logger.info("Y" + posY);
+		logger.info("T" + posT);
+		
+		Pose position = MR.getPose();
+		
+		logger.info("Position" + position);
+		///// LED STRIP //////
+		ledStrip.setDefaultColor(SegmentColor.BLUE, LEDStripUserStatePriority.USER);
+		ledStrip.showUserEffect();
+		ThreadUtil.milliSleep(2000);
+		ledStrip.stopUserEffect();
+		int isCancel = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, informationText, "OK", "Cancel");
+		if (isCancel == 1)
+        {
+			logger.info("cancelled");
+        } else {
+        	logger.info("continuing");
+        }
+		
+//		//Charges with floor contacts for 10 minutes.
+//		int timeoutInMinutes = 60; 
+//		IFloorMountedChargeCapability chargeCapability = kmp.getCapability(IFloorMountedChargeCapability.class);
+//		chargeCapability.enableCharge(ChargingType.FOR_GIVEN_TIME,timeoutInMinutes);
 	}
 }
 

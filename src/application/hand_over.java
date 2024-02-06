fb75699263f289c0a292dc00e2fb524ccbd56137
kuka <kuka@DESKTOP-JBLU9TO>
2024-02-05 20:45:33 -0800

    --- automatic rebase to remote; old local SHA: 303334f9cdd40b2dbe59a94a8b64c2515e4a6adb ---

--- src/application/hand_over.java
+++ src/application/hand_over.java
@@ -145,7 +145,7 @@ public class hand_over extends RoboticsAPIApplication {
 			
 			CartesianSineImpedanceControlMode sineMode;
 			sineMode = CartesianSineImpedanceControlMode.createSinePattern(CartDOF.X, 2.0, 50.0, 500.0);
-			robot.move(linRel(0.0, 150.0,0.0).setCartVelocity(100).setMode(sineMode));
+			robot.move(linRel(0.0,0.0,0.0).setCartVelocity(100).setMode(sineMode));
 			
 			//IMotionContainer m1 = robot.moveAsync(positionHold(sineMode, 20, TimeUnit.SECONDS));
 			Frame pose = robot.getCurrentCartesianPosition(robot.getFlange());

070628d48eb28b24d608aed90d16b2ea1f8d43bb
kuka <kuka@DESKTOP-JBLU9TO>
2024-02-06 18:30:13 -0800

    --- automatic rebase to remote; old local SHA: afaaa5fe93df601be02320a89b9e341f95d75ee7 ---

--- src/application/timed.java
+++ src/application/timed.java
@@ -86,13 +86,14 @@ public class timed {
 	private Pair<ArrayList<Double>  , ArrayList<Double>> distance(Frame pose, ArrayList<Double> velocity, ArrayList<Double> acceleration) {
 	    Frame newPosition = robot.getCurrentCartesianPosition(gripper.getFrame("/TCP"));
 	    Vector3D distance = new Vector3D((pose.getX() - newPosition.getX()), (pose.getY() - newPosition.getY()), (pose.getZ() - newPosition.getZ()));
-	    double currentVel = distance.length();  
+	    double currentVel = distance.length()*10;  
 	    velocity.add(currentVel);
 	    double accel = 0.0;
 	    if (velocity.size() > 9) {
 	    	for (int i = 1; i<10;i++){
 	    		accel -= velocity.get(velocity.size()-i);
 	    	}
+	    	accel = accel/9;
 	    	acceleration.add(accel);
 	    } else {
 	    	acceleration.add(accel);
@@ -118,7 +119,7 @@ public class timed {
 	    		pose.add(robot.getCurrentCartesianPosition(gripper.getFrame("/TCP")));
 	        }
 		};
-		beeperHandle = scheduler.scheduleAtFixedRate(beeper, 10, 1000, TimeUnit.SECONDS);
+		beeperHandle = scheduler.scheduleAtFixedRate(beeper, 10, 100, TimeUnit.MILLISECONDS);
 		scheduler.schedule(new Runnable() {public void run() { beeperHandle.cancel(true); }}, 60 * 3, TimeUnit.SECONDS);
 	}
 	

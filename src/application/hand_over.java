9c77acd7d7cae1d4999a911c11f0979384ebaa3e
kuka <kuka@DESKTOP-JBLU9TO>
2024-02-06 18:14:26 -0800

    --- automatic rebase to remote; old local SHA: 421d4830001e2fec2fd983eabf452ed88dea1b9d ---

--- src/application/hand_over.java
+++ src/application/hand_over.java
@@ -156,7 +156,6 @@ public class hand_over extends RoboticsAPIApplication {
 			lissajousMode.parametrize(CartDOF.B).setStiffness(100);
 			lissajousMode.parametrize(CartDOF.C).setStiffness(100);
 			lissajousMode.parametrize(CartDOF.X).setStiffness(400);
-			lissajousMode.setMaxCartesianVelocity(70, 70, 70, 70, 70, 70);
 			IMotionContainer m1 = robot.moveAsync(positionHold(lissajousMode, 20, TimeUnit.SECONDS));
 			Frame pose = robot.getCurrentCartesianPosition(robot.getFlange());
 			logger.info("Please take the object!");

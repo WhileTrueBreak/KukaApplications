1377f65d10b25452a28e576891d842aad903c2a0
kuka <kuka@DESKTOP-JBLU9TO>
2024-02-06 18:24:36 -0800

    --- automatic rebase to remote; old local SHA: ca1802d13f5bbf0471d3f15fc2f34ebb19051c87 ---

---
+++ src/application/velocityTest.java
@@ -0,0 +1,135 @@
+package application;
+ 
+import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
+import javax.inject.Named;
+//import com.kuka.math.geometry.Vector3D;
+import com.kuka.nav.command.CommandContainer;
+import com.kuka.nav.geometry.Vector2D;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.capabilities.honk.IHonkCapability;
+import com.kuka.roboticsAPI.conditionModel.BooleanIOCondition;
+import com.kuka.roboticsAPI.conditionModel.ForceCondition;
+import com.kuka.roboticsAPI.conditionModel.ICallbackAction;
+import com.kuka.roboticsAPI.conditionModel.ICondition;
+import com.kuka.roboticsAPI.conditionModel.ITriggerAction;
+import com.kuka.roboticsAPI.deviceModel.JointPosition;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.deviceModel.kmp.SunriseOmniMoveMobilePlatform;
+import com.kuka.roboticsAPI.executionModel.CommandInvalidException;
+import com.kuka.roboticsAPI.executionModel.IFiredTriggerInfo;
+import com.kuka.roboticsAPI.geometricModel.CartDOF;
+import com.kuka.roboticsAPI.geometricModel.CartPlane;
+import com.kuka.roboticsAPI.geometricModel.Frame;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.World;
+import com.kuka.roboticsAPI.geometricModel.math.Transformation;
+import com.kuka.roboticsAPI.geometricModel.math.Vector;
+import com.kuka.roboticsAPI.motionModel.IMotion;
+import com.kuka.roboticsAPI.motionModel.IMotionContainer;
+import com.kuka.roboticsAPI.motionModel.RelativeLIN;
+import com.kuka.roboticsAPI.motionModel.SplineMotionCP;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianSineImpedanceControlMode;
+import com.kuka.roboticsAPI.sensorModel.ForceSensorData;
+import com.kuka.roboticsAPI.sensorModel.TorqueSensorData;
+import com.kuka.task.ITaskLogger;
+import com.kuka.common.ThreadUtil;
+import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
+import com.vividsolutions.jts.math.Vector3D;
+ 
+import static com.kuka.roboticsAPI.motionModel.HRCMotions.*;
+ 
+
+/**
+* Implementation of a robot application.
+* <p>
+* The application provides a {@link RoboticsAPITask#initialize()} and a 
+* {@link RoboticsAPITask#run()} method, which will be called successively in 
+* the application lifecycle. The application will terminate automatically after 
+* the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+* task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+* exception is thrown during initialization or run. 
+* <p>
+* <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+* {@link RoboticsAPITask#dispose()} method.</b> 
+* 
+* @see UseRoboticsAPIContext
+* @see #initialize()
+* @see #run()
+* @see #dispose()
+*/
+public class velocityTest extends RoboticsAPIApplication {
+	@Inject
+	private LBR robot;
+	@Inject 
+	private Gripper2F gripper2F1;
+	@Inject
+	private MediaFlangeIOGroup mF;
+	@Inject
+	@Named("RobotiqGripper")
+	private Tool gripper;
+	@Inject
+	private ITaskLogger logger;
+	@Inject
+	private timed cartData;
+	CartesianImpedanceControlMode springRobot;
+	IMotionContainer m1;
+ 
+	@Override
+	public void initialize() {
+		gripper.attachTo(robot.getFlange());
+		gripper2F1.initalise();
+		gripper2F1.setForce(10);
+		gripper2F1.setSpeed(150);
+		gripper2F1.setPos(100);
+		mF.setLEDBlue(true);
+		ThreadUtil.milliSleep(200);
+		gripper2F1.close();
+		mF.setLEDBlue(false);
+		ThreadUtil.milliSleep(200);
+		cartData.initialize();
+		cartData.run();
+		//Spring motion initialisation
+		springRobot = new CartesianImpedanceControlMode(); 
+		springRobot.parametrize(CartDOF.X).setStiffness(200); 
+		springRobot.parametrize(CartDOF.C).setStiffness(100);
+		springRobot.parametrize(CartDOF.B).setStiffness(100);
+		springRobot.parametrize(CartDOF.A).setStiffness(100);
+		springRobot.setReferenceSystem(World.Current.getRootFrame());
+		springRobot.parametrize(CartDOF.ALL).setDamping(0.7);
+		//USAGE, will move to next line when triggered
+		//LOOK at pipecutting.java for examples on analysing the break condition. 
+		//gripper.move(linRel(0, 0, -30, World.Current.getRootFrame()).setCartVelocity(50).breakWhen(touch10));
+ 
+
+	}
+	@Override
+	public void run() {
+//		IHonkCapability honkCapability = kmp.getCapability(IHonkCapability.class);
+//		honkCapability.honk();
+		double velocity = cartData.vel;
+		double acceleration = cartData.acc;
+		logger.info("arm velocity :" + velocity);
+		logger.info("arm acceleration :" + acceleration);
+		IMotionContainer m = robot.moveAsync(ptp(getApplicationData().getFrame("/P2")).setJointVelocityRel(0.1).setJointAccelerationRel(0.05).setMode(springRobot));
+		while (true){
+			velocity = cartData.vel;
+			acceleration = cartData.acc;
+			logger.info("arm velocity :" + velocity);
+			logger.info("arm acceleration :" + acceleration);
+			ThreadUtil.milliSleep(1000);
+			if (m1.isFinished()){
+				break;
+			}
+			
+		}
+		ThreadUtil.milliSleep(500);
+		velocity = cartData.vel;
+		acceleration = cartData.acc;
+		logger.info("arm velocity :" + velocity);
+		logger.info("arm acceleration :" + acceleration);
+	}
+}

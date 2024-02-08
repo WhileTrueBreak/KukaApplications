75cf6a640ce91b5e6d58107c8fab395f44b1f409
kuka <kuka@DESKTOP-JBLU9TO>
2024-02-07 21:15:20 -0800

    --- automatic rebase to remote; old local SHA: 065276cf0a30f14bff2372085e6b9dce93109041 ---

--- src/application/window.java
+++ src/application/window.java
@@ -173,36 +173,36 @@ public class window extends RoboticsAPIApplication{
 		away.setGammaRad(window.getGammaRad());
 		
 		handle.setX(window.getX());  // ---> 
-		handle.setY(window.getY()-48); // ---> 0 put X values
+		handle.setY(window.getY()+48); // ---> 0 put X values
 		handle.setZ(window.getZ()-39);
 		handle.setAlphaRad(window.getAlphaRad());
 		handle.setBetaRad(window.getBetaRad());
 		handle.setGammaRad(window.getGammaRad());
 		
-		lock1.setX(window.getX()+29);
-		lock1.setY(window.getY()-14);
-		lock1.setZ(window.getZ()-118);
+		lock1.setX(window.getX()-118);
+		lock1.setY(window.getY()+14);
+		lock1.setZ(window.getZ()-29);
 		lock1.setAlphaRad(window.getAlphaRad());
 		lock1.setBetaRad(window.getBetaRad());
 		lock1.setGammaRad(window.getGammaRad());
 		
-		lock2.setX(window.getX()-19);
+		lock2.setX(window.getX()-154);
 		lock2.setY(window.getY()+19);
-		lock2.setZ(window.getZ()-154);
+		lock2.setZ(window.getZ()-19.6);
 		lock2.setAlphaRad(window.getAlphaRad());
 		lock2.setBetaRad(window.getBetaRad());
 		lock2.setGammaRad(window.getGammaRad());
 		
-		lock3.setX(window.getX()-29);
-		lock3.setY(window.getY()+27);
-		lock3.setZ(window.getZ()-180);
+		lock3.setX(window.getX()-180);
+		lock3.setY(window.getY()+28);
+		lock3.setZ(window.getZ()-26);
 		lock3.setAlphaRad(window.getAlphaRad());
 		lock3.setBetaRad(window.getBetaRad());
 		lock3.setGammaRad(window.getGammaRad());
 		
-		lock4.setX(window.getX()-55);
+		lock4.setX(window.getX()-211);
 		lock4.setY(window.getY()+44);
-		lock4.setZ(window.getZ()-211);
+		lock4.setZ(window.getZ()-55);
 		lock4.setAlphaRad(window.getAlphaRad());
 		lock4.setBetaRad(window.getBetaRad());
 		lock4.setGammaRad(window.getGammaRad());
@@ -220,6 +220,9 @@ public class window extends RoboticsAPIApplication{
 		gripper.move(ptp(window).setJointVelocityRel(0.3));
 		logger.info("test 1.1");
 		gripper.move(ptp(lock1).setJointVelocityRel(0.3));
+		gripper.move(ptp(lock2).setJointVelocityRel(0.3));
+		gripper.move(ptp(lock3).setJointVelocityRel(0.3));
+		gripper.move(ptp(lock4).setJointVelocityRel(0.3));
 		logger.info("test 1.11");
 		gripper.move(ptp(handle).setJointVelocityRel(0.3));
 		logger.info("test2");

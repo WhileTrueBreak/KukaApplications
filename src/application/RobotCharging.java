52a0d166e0070d2bcccd1f5389bf9b2ecbf24c46
k3ppp <102340397+k3ppp@users.noreply.github.com>
2024-03-07 14:16:21 +1100

    --- automatic rebase to remote; old local SHA: c87aba855977f2efe6dcfbe897b36a9ad27b213f ---

--- src/application/RobotCharging.java
+++ src/application/RobotCharging.java
@@ -1,81 +1,53 @@
 package application;
- 
- 
-import java.util.Map;
- 
+
+
 import javax.inject.Inject;
- 
-import com.kuka.generated.ioAccess.RemoteControlIOGroup;
-import com.kuka.nav.Pose;
-import com.kuka.nav.data.LocationData;
-import com.kuka.nav.robot.MobileRobot;
-import com.kuka.resource.IResourceManager;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
- 
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
 import com.kuka.roboticsAPI.capabilities.floorMountedCharge.ChargingType;
 import com.kuka.roboticsAPI.capabilities.floorMountedCharge.IFloorMountedChargeCapability;
-import com.kuka.roboticsAPI.controllerModel.sunrise.ISafetyState;
-import com.kuka.roboticsAPI.deviceModel.OperationMode;
+import com.kuka.roboticsAPI.capabilities.interfaces.ICapability;
+import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.deviceModel.kmp.KmpOmniMove;
 import com.kuka.roboticsAPI.deviceModel.kmp.SunriseOmniMoveMobilePlatform;
-import com.kuka.roboticsAPI.geometricModel.LoadData;
-import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
-import com.kuka.task.ITaskLogger;
-import com.kuka.roboticsAPI.motionModel.kmp.MobilePlatformPosition;
-import com.kuka.roboticsAPI.motionModel.kmp.MobilePlatformRelativeMotion;
- 
+
 /**
-* Implementation of a robot application.
-* <p>
-* The application provides a {@link RoboticsAPITask#initialize()} and a 
-* {@link RoboticsAPITask#run()} method, which will be called successively in 
-* the application lifecycle. The application will terminate automatically after 
-* the {@link RoboticsAPITask#run()} method has finished or after stopping the 
-* task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
-* exception is thrown during initialization or run. 
-* <p>
-* <b>It is imperative to call <code>super.dispose()</code> when overriding the 
-* {@link RoboticsAPITask#dispose()} method.</b> 
-* 
-* @see UseRoboticsAPIContext
-* @see #initialize()
-* @see #run()
-* @see #dispose()
-*/
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
 public class RobotCharging extends RoboticsAPIApplication {
- 
-//	@Inject
-//	private KmpOmniMove kmp1;
- 
 	@Inject
-	private MobileRobot MR;
+	private LBR lBR_iiwa_14_R820_1;
+
 	@Inject
 	private SunriseOmniMoveMobilePlatform kmp;
-	@Inject
-	private MobilePlatformPosition MPM;
 
-	@Inject
-	private ITaskLogger logger;
-
-//	@Inject
-//   private LocationData locData;
-//	
-//	@Inject
-//	private RemoteControlIOGroup RCIO;
-	private final static String informationText=
-	         "Robot going to charge!"+ "\n" +
-				"\n" +
-				"press cancel to cancel.";
 	@Override
 	public void initialize() {
 		// initialize your application here
 	}
- 
+
 	@Override
 	public void run() {
-		int timeoutInMinutes = 60; 
+		// Charges with floor contacts for 10 minutes.
+		int timeoutInSeconds = 600; // value in seconds
 		IFloorMountedChargeCapability chargeCapability = kmp.getCapability(IFloorMountedChargeCapability.class);
-		chargeCapability.enableCharge(ChargingType.FOR_GIVEN_TIME,timeoutInMinutes);
+		chargeCapability.enableCharge(ChargingType.FOR_GIVEN_TIME,timeoutInSeconds);
+		// kmp.enableCharge(ChargingType.FOR_GIVEN_TIME, timeoutInSeconds);
 	}
 }
- 

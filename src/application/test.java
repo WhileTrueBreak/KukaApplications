d4d0d2860346d2cf30fce30d317d224a0b8841ab
kuka <jeff.jc2003@gmail.com>
2024-07-08 09:58:32 +1000

    --- automatic rebase to remote; old local SHA: 9c4e040d8e18d00d27185cbb102e87c970b85308 ---

--- src/application/test.java
+++ src/application/test.java
@@ -2,10 +2,37 @@ package application;
 
 import java.io.IOException;
 
-public class test {
+import javax.inject.Inject;
+import javax.inject.Named;
 
-	public static void main(String[] args) throws InterruptedException, IOException {
+import com.kuka.generated.ioAccess.Gripper2FIOGroup;
+import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.task.ITaskLogger;
+
+public class test extends RoboticsAPIApplication{
+
+	@Inject
+	private LBR robot;
+	@Inject
+	private Gripper2FIOGroup gripperIO;
+	@Inject
+	private MediaFlangeIOGroup mF;
+	@Inject
+	@Named("RobotiqGripper")
+	private Tool tool;
+	@Inject
+	private ITaskLogger logger;
+	
+	@Override
+	public void initialize() {
 		
+	}
+	
+	@Override
+	public void run() throws Exception {
 		
 	}
 

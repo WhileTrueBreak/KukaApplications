8eb0547346dc267ecfb0c22449f9494a09099869
kuka <jeff.jc2003@gmail.com>
2024-07-11 11:21:09 +1000

    --- automatic rebase to remote; old local SHA: 8bc96bf560297e4f9dc57d53bd82f01d239378e2 ---

--- src/application/QuickControl.java
+++ src/application/QuickControl.java
@@ -106,7 +106,11 @@ public class QuickControl extends RoboticsAPIApplication{
 	
 	private boolean moveToPos(double[] pos) {
 		try {
-			tool.move(BasicMotions.ptp(new JointPosition(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], pos[6])));
+			queuedMotions.add(tool.moveAsync(BasicMotions.ptp(new JointPosition(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], pos[6])).setBlendingRel(1)));
+			if(queuedMotions.size() > 4){
+				queuedMotions.get(0).cancel();
+				queuedMotions.remove(0);
+			}
 		} catch (Exception e) {
 			return false;
 		}

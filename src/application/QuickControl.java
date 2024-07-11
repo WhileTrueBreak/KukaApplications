2827c99594abebee1e632edc61d3ebce12030c16
keena <keena@DESKTOP-JBLU9TO>
2024-07-11 15:58:50 +1000

    --- automatic rebase to remote; old local SHA: c7b3a75e236a341c68e08d3188b610b06c52d5db ---

--- src/application/QuickControl.java
+++ src/application/QuickControl.java
@@ -105,10 +105,10 @@ public class QuickControl extends RoboticsAPIApplication{
 	
 	private boolean moveToPos(double[] pos) {
 		try {
-			queuedMotions.add(tool.moveAsync(BasicMotions.ptp(new JointPosition(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], pos[6]))
-					.setJointVelocityRel(0.4)));
-			if(queuedMotions.size() > 3){
-				queuedMotions.get(0).cancel();
+			queuedMotions.add(tool.move(BasicMotions.ptp(new JointPosition(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], pos[6]))
+					.setJointAccelerationRel(0.5)));
+			if(queuedMotions.size() > 2){
+				queuedMotions.get(0).await();
 				queuedMotions.remove(0);
 			}
 		} catch (Exception e) {
